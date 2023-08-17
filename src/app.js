92fd97d69477d23611df872cf62298d686883317
WillyDeww <willydeww@gmail.com>
2023-08-17 12:35:57 -0400

    Created a json
    created json that goes to /api/customers

--- src/app.js
+++ src/app.js
@@ -2,8 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const customers = [
+    {
+        "name": "Will",
+        "industry": "music"
+    },
+    {
+        "name": "Will",
+        "industry": "music"
+    },
+    {
+        "name": "Will",
+        "industry": "music"
+    }
+]; 
+
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Welcome!');
+});
+
+app.get('/api/customers', (req, res) => {
+    res.send({"customers": customers});
 });
 
 app.post('/', (req, res) => {
